8927ad39e8488c85775b6f91e56745bfca1b4876
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-08-09 22:01:42 +0630

    Add site configuration for personal profile

---
+++ js/config.js
@@ -0,0 +1,23 @@
+const SITE_CONFIG = {
+  name: "Moe Kyaw Aung",
+  role: "Senior Android Developer & Full-Stack Engineer",
+
+  location: "Tachileik, Myanmar ↔ Bangkok, Thailand",
+
+  gravatar:
+    "https://gravatar.com/moekyawaung2026",
+
+  profileImage:
+    "https://res.cloudinary.com/dye5qpwii/image/upload/v1778527878/IMG_20260430_053105_uef0yr.png",
+
+  github:
+    "https://github.com/Dev-moe-kyawaung",
+
+  phone: [
+    "+95 9 889 000 889",
+    "+95 9 666 000 050"
+  ],
+
+  email:
+    "moekyawaung@programmer.net"
+};

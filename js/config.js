9aa1a610ffc7b4dfd588bc954c22022ed5a02328
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-08-09 22:08:24 +0630

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,23 +1,35 @@
-const SITE_CONFIG = {
+window.SITE_CONFIG = {
   name: "Moe Kyaw Aung",
+  burmeseName: "မိုးကျော်အောင်",
+
   role: "Senior Android Developer & Full-Stack Engineer",
 
-  location: "Tachileik, Myanmar ↔ Bangkok, Thailand",
-
-  gravatar:
-    "https://gravatar.com/moekyawaung2026",
+  location:
+    "Tachileik, Myanmar ↔ Bangkok, Thailand",
 
   profileImage:
     "https://res.cloudinary.com/dye5qpwii/image/upload/v1778527878/IMG_20260430_053105_uef0yr.png",
 
+  gravatarProfile:
+    "https://gravatar.com/moekyawaung2026",
+
+  gravatarSlug:
+    "moekyawaung2026",
+
   github:
     "https://github.com/Dev-moe-kyawaung",
 
-  phone: [
+  email:
+    "moekyawaung@programmer.net",
+
+  phones: [
     "+95 9 889 000 889",
     "+95 9 666 000 050"
   ],
 
-  email:
-    "moekyawaung@programmer.net"
+  resume:
+    "assets/documents/moe-kyaw-aung-resume.pdf",
+
+  socialFallbackAvatar:
+    "https://res.cloudinary.com/dye5qpwii/image/upload/v1778527878/IMG_20260430_053105_uef0yr.png"
 };

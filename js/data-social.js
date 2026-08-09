222cd9115241657216b469debf5cb616f88a8c3d
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-08-09 22:11:03 +0630

    Add SOCIAL_LINKS array with social media links

---
+++ js/data-social.js
@@ -0,0 +1,52 @@
+window.SOCIAL_LINKS = [
+  {
+    label: "GitHub",
+    url: "https://github.com/Dev-moe-kyawaung",
+    icon: "⌘"
+  },
+  {
+    label: "LinkedIn",
+    url: "https://www.linkedin.com/in/moe-kyaw-aung-2653093a1",
+    icon: "in"
+  },
+  {
+    label: "YouTube",
+    url: "https://www.youtube.com/channel/UCuTXUguZb4xjeL2nX8WJG",
+    icon: "▶"
+  },
+  {
+    label: "Bluesky",
+    url: "https://bsky.app/profile/moekyawaung96.bsky.social",
+    icon: "✦"
+  },
+  {
+    label: "Tumblr",
+    url: "https://www.tumblr.com/moekyawaung",
+    icon: "t"
+  },
+  {
+    label: "Flickr",
+    url: "https://www.flickr.com/people/204037451@N06",
+    icon: "●"
+  },
+  {
+    label: "Vimeo",
+    url: "https://vimeo.com/user252414232",
+    icon: "v"
+  },
+  {
+    label: "Gravatar",
+    url: "https://gravatar.com/moekyawaung13721",
+    icon: "◉"
+  },
+  {
+    label: "PayPal",
+    url: "https://www.paypal.com/paypalme/my/profile",
+    icon: "P"
+  },
+  {
+    label: "Slack",
+    url: "https://moekyawaung.slack.com/",
+    icon: "#"
+  }
+];
